8ce393bb9095b3f532b8a9eb6bdc83eeafb1bc15
meriemsakhri <meriem.sakhri@polytechnicien.tn>
2025-10-21 17:01:11 +0100

    add TP5 Part1 - registration form UI

---
+++ app/src/main/java/com/example/tp_android/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.example.tp_android;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
