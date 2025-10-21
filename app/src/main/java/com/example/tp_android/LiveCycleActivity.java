0c0df19b37ceb91b506a09ae3bcdf631715e83b9
meriemsakhri <meriem.sakhri@polytechnicien.tn>
2025-10-21 16:25:31 +0100

    Implemented counter with increment button/preserve counter on orientation change/add landscape layout

--- app/src/main/java/com/example/tp_android/LiveCycleActivity.java
+++ app/src/main/java/com/example/tp_android/LiveCycleActivity.java
@@ -2,47 +2,84 @@ package com.example.tp_android;
 
 import android.os.Bundle;
 import android.util.Log;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class LiveCycleActivity extends AppCompatActivity {
+
+    private static final String TAG = "LifeCycle";
+    private static final String KEY_COMPTEUR = "compteur"; // Clé pour sauvegarder le compteur
+    private TextView txtCompteur;
+    private Button btnIncrementer;
+    private int cpt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_live_cycle);
-        Log.e("LiveCycleActivity", "On est dans l'état onCreate");
-    }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.e("LiveCycleActivity", "On est dans l'état onPause");
-    }
+        txtCompteur = findViewById(R.id.TXT_Compteur);
+        btnIncrementer = findViewById(R.id.BTN_Incrementer);
 
+        if (savedInstanceState != null) {
+            cpt = savedInstanceState.getInt(KEY_COMPTEUR, 0);
+            txtCompteur.setText("Compteur = " + cpt);
+
+        }
+        // Gestion du clic sur le bouton
+        btnIncrementer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cpt++;
+                txtCompteur.setText("Compteur = " + cpt);
+            }
+        });
+
+        Log.i(TAG, "Activité onCreate");
+    }
     @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e("LiveCycleActivity", "On est dans l'état onResume");
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt(KEY_COMPTEUR, cpt);
+        Log.i(TAG, "SAUVEGARDE: cpt = " + cpt);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.e("LiveCycleActivity", "On est dans l'état onStart");
+        Log.i(TAG, "Activité onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "Activité onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.i(TAG, "Activité onPause");
+        super.onPause();
     }
 
     @Override
     protected void onStop() {
+        Log.i(TAG, "Activité onStop");
         super.onStop();
-        Log.e("LiveCycleActivity", "On est dans l'état onStop");
     }
 
     @Override
     protected void onDestroy() {
+        Log.i(TAG, "Activité onDestroy");
         super.onDestroy();
-        Log.e("LiveCycleActivity", "On est dans l'état onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "Activité onRestart");
     }
 }
